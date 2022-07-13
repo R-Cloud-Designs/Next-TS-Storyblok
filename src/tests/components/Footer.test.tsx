da3f1037e127ea759fef140a7d9bb8b7201991a6
Rachid Angelista <rachid.ang@sounds.global>
2022-07-13 02:35:22 +0200

    installed and configred vitest and added a stub test

---
+++ src/tests/components/Footer.test.tsx
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("Storyblok Footer component testing", () => {
+  test("Component renders on screen", () => {
+    expect(true).toBe(true);
+  });
+});
