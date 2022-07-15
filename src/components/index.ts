453f33d68d8d5d1aeb52698a9e9fd95ca496d828
Rachid Angelista <rachid.ang@sounds.global>
2022-07-15 18:14:14 +0200

    Updated component structure and fixed build errors

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import Footer from "./sbComponents/Common/Footer";
+import Grid from "./sbComponents/Common/Grid";
+import Page from "./sbComponents/Common/Page";
+import Paragraph from "./sbComponents/Common/Paragraph";
+import Teaser from "./sbComponents/Common/Teaser";
+import NextCard from "./sbComponents/NextCard/NextCard";
+import SplitCarouselView from "./sbComponents/SplitCarouselView/SplitCarouselView";
+
+export const storyblokComponents = {
+  Grid: Grid,
+  Teaser: Teaser,
+  Page: Page,
+  NextCard: NextCard,
+  Paragraph: Paragraph,
+  Footer: Footer,
+  SplitCarouselView: SplitCarouselView,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,28 +2,12 @@ import "../styles/globals.css";
 import "../styles/app.css";
 import type { AppProps } from "next/app";
 import { storyblokInit, apiPlugin } from "@storyblok/react";
-import Grid from "../components/sbComponents/Common/Grid";
-import Teaser from "../components/sbComponents/Common/Teaser";
-import Page from "../components/sbComponents/Common/Page";
-import NextCard from "../components/sbComponents/NextCard/NextCard";
-import Paragraph from "../components/sbComponents/Common/Paragraph";
-import Footer from "../components/sbComponents/Common/Footer";
-import SplitCarouselView from "../components/sbComponents/SplitCarouselView/SplitCarouselView";
-
-const components = {
-  Grid: Grid,
-  Teaser: Teaser,
-  Page: Page,
-  NextCard: NextCard,
-  Paragraph: Paragraph,
-  Footer: Footer,
-  SplitCarouselView: SplitCarouselView,
-};
+import { storyblokComponents } from "../components";
 
 storyblokInit({
   accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
   use: [apiPlugin],
-  components,
+  components: storyblokComponents,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
